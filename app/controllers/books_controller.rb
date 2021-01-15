a97c5163f585d31da73735697ea71135821113d9
panna001 <sonar.pocket.0926@gmail.com>
2021-01-15 08:04:09 +0000

    [Add] レンダー、メッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,15 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    # 間違っている可能性大　エラーは出ず
-    redirect_to user_path(current_user)
+    @book.user_id = current_user.id
+    if @book.save
+    # 間違っている可能性有り　エラーは出ず。引数current_user注意
+      redirect_to user_path(current_user), notice: "You have created book successfully."
+    else
+      # titleの入力が再表示されない
+      @user = User.find(current_user.id)
+      render "users/show"
+    end
   end
 
   def index
@@ -25,7 +31,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:name, :body)
+    params.require(:book).permit(:title, :body)
   end
 
 end
