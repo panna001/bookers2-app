8fb0bb0a035308888119d27035246ba9b5ed257c
panna001 <sonar.pocket.0926@gmail.com>
2021-02-02 09:17:17 +0000

    [Add]リレーションシップroute

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+  post "follow/:id" => "relationships#follow", as: "follow"
+  post "unfollow/:id" => "relationships#unfollow", as: "unfollow"
 end
