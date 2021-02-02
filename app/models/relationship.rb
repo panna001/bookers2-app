2e648cc57506b2921050a336227dda25157b07b5
panna001 <sonar.pocket.0926@gmail.com>
2021-02-02 09:33:37 +0000

    [Add]リレーションシップcontroller

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,18 +1,5 @@
 class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
-  belongs_to :following_user, through: :follower, source: :followed
-  belongs_to :follower_user, through: :followed, source: :follower
   
-  def follow(user_id)
-    follower.create(followed_id: user_id)
-  end
-  
-  def unfollow(user_id)
-    follower.find_by(followed_id: user_id).destroy    
-  end
-  
-  def following?(user)
-    following_user.include?(user)
-  end
 end
