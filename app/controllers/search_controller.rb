66d657f22d0ea641de7e592c657c4a2a7e94ede7
panna001 <sonar.pocket.0926@gmail.com>
2021-02-03 11:33:50 +0000

    [Add]検索機能いったん完成

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,33 @@
 class SearchController < ApplicationController
   def search
+    @model = params[:search][:model]
+    @key_word = params[:search][:key_word]
+
     if params[:search][:model] == "User"
-      @users = User.where("name like ?", "%#{params[:search][:text]}%")
-    else
-      @books = Book.where("title like ?", "%#{params[:search][:text]}%")
+      if @key_word == ""
+        @users = User.all
+      elsif  params[:search][:condition] == "部分一致"
+        @users = User.where("name like ? OR introduction like ?", "%#{params[:search][:key_word]}%", "%#{params[:search][:key_word]}%")
+      elsif params[:search][:condition] == "完全一致"
+        @users = User.where("name like ? OR introduction like ?", "#{params[:search][:key_word]}", "#{params[:search][:key_word]}")
+      elsif params[:search][:condition] == "前方一致"
+        @users = User.where("name like ? OR introduction like ?", "#{params[:search][:key_word]}%", "#{params[:search][:key_word]}%")
+      elsif params[:search][:condition] == "後方一致"
+        @users = User.where("name like ? OR introduction like ?", "%#{params[:search][:key_word]}", "%#{params[:search][:key_word]}")
+      end
+    elsif params[:search][:model] == "Book"
+      if @key_word == ""
+        @books = Book.all
+      elsif params[:search][:condition] == "部分一致"
+        @books = Book.where("title like ? OR body like ?", "%#{params[:search][:key_word]}%", "%#{params[:search][:key_word]}%")
+      elsif params[:search][:condition] == "完全一致"
+        @books = Book.where("title like ? OR body like ?", "#{params[:search][:key_word]}", "#{params[:search][:key_word]}")
+      elsif params[:search][:condition] == "前方一致"
+        @books = Book.where("title like ? OR body like ?", "#{params[:search][:key_word]}%", "#{params[:search][:key_word]}%")
+      elsif params[:search][:condition] == "後方一致"
+        @books = Book.where("title like ? OR body like ?", "%#{params[:search][:key_word]}", "%#{params[:search][:key_word]}")
+      end
     end
+
   end
 end
