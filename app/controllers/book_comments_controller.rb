dac00680aec2593edf7ac05680b9e4314c4ecc43
panna001 <sonar.pocket.0926@gmail.com>
2021-02-04 15:50:13 +0000

    [Finish]課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,18 +1,18 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    book_comment = current_user.book_comments.new(book_comment_params)
-    book_comment.book_id = book.id
-    book_comment.save
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.save
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    book_comment = current_user.book_comments.find_by(book_id: book.id)
-    book_comment.destroy
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.find_by(book_id: @book.id)
+    @book_comment.destroy
+    # redirect_back(fallback_location: root_path)
   end
 
   private
